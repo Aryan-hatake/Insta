287efb4319889dc4a41681db68fb42570616daa9
Aryan <aryangadhave202@gmail.com>
2026-02-11 00:44:53 +0530

    Auth_Apis_finalised

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,101 @@
+const userModel = require("../models/user.model")
+const crypto = require('crypto')
+const jwt = require("jsonwebtoken")
+
+
+async function registerController(req, res) {
+    const { email, password, username } = req.body;
+
+    const userExist = await userModel.findOne({
+        $or: [
+            {
+                email
+            },
+            {
+                username
+            }
+        ]
+    })
+
+    console.log(userExist)
+
+    if(userExist){
+       return  res.status(409).json({
+            success:false,
+            message:userExist.email === email ?  "User with same Email exist":"Username is not available"
+        })
+    }
+   
+    const user = await userModel.create({
+        email,
+        username,
+        password:crypto.createHash("sha256").update(password).digest("hex")
+    })
+
+
+    const token = jwt.sign(
+        {
+            id:user._id,
+            email:user.email
+        },
+        process.env.JWT_SECRET,
+        {expiresIn:"1h"}
+    )
+    
+    res.cookie("token",token)
+
+    res.status(201).json({
+
+        success:true,
+        message:"User has been registered successfully",
+        token
+
+
+    })
+
+    
+}
+async function loginController(req,res){
+
+    const {email,password,username} = req.body
+
+    const user = await userModel.findOne({
+        $or:[
+            {email},
+            {username}
+        ]
+    })
+
+    if(!user){
+        return res.status(404).json({
+            success:false,
+            message:"User does not exist"
+        })
+    }
+
+    isPasswordValid = user.password === crypto.createHash("sha256").update(password).digest("hex");
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            success:false,
+            message:"Invalid Password"
+        })
+    }
+
+    const token = jwt.sign(
+        {
+            id:user._id,
+            email:user.email
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("token",token)
+
+    res.status(200).json({
+        success:true,
+        message:"User Logged in successfully"
+    })
+}
+
+module.exports = {registerController,loginController}
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        unique:[true,"User with same email already exist"],
+        required:true
+    },
+    username:{
+        type:String,
+        unique:[true,"Username not available"],
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    followers:Array,
+    bio:String,
+    profile_img:{
+        type:String,
+        default:"https://ik.imagekit.io/2chp5tqv4/download.jpg"
+    }
+})
+
+const userModel = mongoose.model("user",userSchema)
+
+module.exports = userModel
